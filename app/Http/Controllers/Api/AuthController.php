de9014cf5e4a3c63558fd77895455c282a4c3091
unknown <samuelfrodriguezjr@gmail.com>
2020-10-15 01:57:49 +0800

    Updated on Laravel 8 and can register user

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+class AuthController extends Controller
+{
+    public function login(){
+        echo 'Login Endpoint Requested';
+    }
+
+    public function signup(Request $request){
+        
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|confirmed'
+        ]);
+        
+        $user = new User([
+            'name'=>$request->name,
+            'email'=>$request->email,
+            'password'=>bcrypt($request->password)
+        ]);
+
+        $user->save();
+
+        return response()->json([
+            "message" => "User registered successfully"
+        ],201);
+    }
+
+    public function index(){
+        echo 'Hello World';
+    }
+}
